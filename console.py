fc0bba866b31f72d46dc20fc51da23841754dc1f
Abdellah <abdellahtaghouni@gmail.com>
2024-01-12 09:59:37 -0800

    creat function

--- console.py
+++ console.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
+import shlex
+
 
 class HBNBCommand(cmd.Cmd):
     """Interactive command-line console for HBNB data management"""
@@ -18,6 +22,18 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """Quit the console"""
         return True
+
+    def do_create(self, arg):
+         """Create a new instance of BaseModel, save it, and print its id."""
+        args = shlex.split(arg)
+        if not args or args[0] == "":
+            print("** class name missing **")
+        elif args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+        else:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
